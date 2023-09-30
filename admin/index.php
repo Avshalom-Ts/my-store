2e9d6dff59fd5bb8158c9d5ec06f3a90a5eebfed
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-09-30 21:07:39 +0300

    Basic layout for admin page

---
+++ admin/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap CSS link -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="../style.css">
+  <title>Admin Gashboard</title>
+</head>
+
+<body>
+  <!-- Navbar -->
+  <div class="container-fluid p-0">
+    <!-- First child -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+      <div class="container-fluid">
+        <a class="nav-link" href="/my-store/admin/">
+          <img src="../images/AzLogo48px.png" alt="logo" class="logo">
+        </a>
+        <nav class="navbar navbar-expand-lg">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Welcome Guest</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </nav>
+
+    <!-- Second child -->
+    <div class="bg-light">
+      <h3 class="text-center p-2">Manage Details</h3>
+    </div>
+
+    <!-- Third child -->
+    <div class="row">
+      <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+        <div class="p-3">
+          <a href="#" class=""><img src="../images/peach.jpg" alt="" class="admin-image"></a>
+          <p class="text-light text-center">Admin name</p>
+        </div>
+        <div class="button text-center">
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View Products</a></button>
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">All Users</a></button>
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- last child -->
+    <div class="bg-info p-3 text-center footer container-fluid">
+      <p>All rights reserved &#x0040; Avshalom-projects 2023 ab</p>
+    </div>
+  </div>
+
+
+
+  <!-- Botstrap JS link -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
