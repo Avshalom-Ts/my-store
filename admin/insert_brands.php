af5e9591760b65a1c9fc024a1ba2c0e62b352cb4
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-10-01 04:43:07 +0300

    Inserting into brands

--- admin/insert_brands.php
+++ admin/insert_brands.php
@@ -1,10 +1,34 @@
+<?php
+include('../includes/connect.php');
+
+if (isset($_POST['insert_brand'])) {
+  $brand_title = $_POST['brand_title'];
+
+  // Select data from database
+  $select_query = "select * from `brands` where brand_title='$brand_title'";
+  $result_select = mysqli_query($con, $select_query);
+  // number of rows
+  $number = mysqli_num_rows($result_select);
+  if ($number > 0) {
+    echo "<script>alert('This brand alredy in the database.')</script>";
+  } else {
+    $insert_query = "insert into `brands` (brand_title) values ('$brand_title')";
+    $result = mysqli_query($con, $insert_query);
+    if ($result) {
+      echo "<script>alert('Brand has been inserted successfully.')</script>";
+    }
+  }
+}
+
+?>
+
+
 <form action="" method="post" class="mb-2">
   <div class="input-group w-90 mb-2">
     <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-    <input type="text" class="form-control" name="cat_title" placeholder="Insert brands" aria-label="brands">
+    <input type="text" class="form-control" name="brand_title" placeholder="Insert brands" aria-label="brands">
   </div>
   <div class="input-group w-10 mb-2 m-auto">
-    <!-- <input type="submit" class="form-control bg-info" name="insert_cat" value="Insert categories"> -->
-    <button class="bg-info p-2 my-3 border-0">Insert brands</button>
+    <input type="submit" class="bg-info border-0 p-2 my-3" name="insert_brand" value="Insert brands">
   </div>
 </form>
