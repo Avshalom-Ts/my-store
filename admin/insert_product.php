205c049188094780ea6f6f9c765e65abae6c05c5
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-10-02 23:55:08 +0300

    Added products to the database

--- admin/insert_product.php
+++ admin/insert_product.php
@@ -1,6 +1,47 @@
 <?php
 include('../includes/connect.php');
+
+if (isset($_POST['insert_product'])) {
+  $product_title = $_POST['product_title'];
+  $product_description = $_POST['product_description'];
+  $product_keywords = $_POST['product_keywords'];
+  $product_category = $_POST['product_category'];
+  $product_brads = $_POST['product_brads'];
+  $product_price = $_POST['product_price'];
+  $product_status = "true";
+
+  // accessing images
+  $product_image1 = $_FILES['product_image1']['name'];
+  $product_image2 = $_FILES['product_image2']['name'];
+  $product_image3 = $_FILES['product_image3']['name'];
+
+  // accessing images tmp name
+  $temp_image1 = $_FILES['product_image1']['tmp_name'];
+  $temp_image2 = $_FILES['product_image2']['tmp_name'];
+  $temp_image3 = $_FILES['product_image3']['tmp_name'];
+
+
+
+  // Checking empty condition
+  if ($product_title == '' or $product_description == '' or $product_keywords == '' or $product_category == '' or $product_brads == '' or $product_image1 == '' or $product_image2 == '' or $product_image3 == '' or $product_price == '') {
+    echo "<script>alert('please fill all fields!!')</script>";
+    exit();
+  } else {
+    move_uploaded_file($temp_image1, "./product_images/$product_image1");
+    move_uploaded_file($temp_image2, "./product_images/$product_image2");
+    move_uploaded_file($temp_image3, "./product_images/$product_image3");
+
+    //insert query
+    $insert_products = "insert into `products` (product_title,product_description,product_keywords,category_id,brand_id,product_image1,product_image2,product_image3,product_price,date,status) values ('$product_title','$product_description','$product_keywords','$product_category','$product_brads','$product_image1','$product_image2','$product_image3','$product_price',NOW(),'$product_status')";
+    $result_query = mysqli_query($con, $insert_products);
+    if ($result_query) {
+      echo "<script>alert('Successfully inserted the product.')</script>";
+    }
+  }
+}
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -96,12 +137,12 @@ include('../includes/connect.php');
       <!-- Price -->
       <div class="form-outline mb-4 w-50  m-auto">
         <label for="product_price" class="form-label">Product Price</label>
-        <input type="text" name="product_price" id="product_price" class="form-control" placeholder="Enter product price" required>
+        <input type="text" name="product_price" id="product_price" class="form-control" autocomplete="off" placeholder="Enter product price" required="required">
       </div>
 
       <!-- Price -->
       <div class="form-outline mb-4 w-50  m-auto">
-        <input type="submite" name="insert_product" class="btn btn-info mb-3 px-3" value="insert Product">
+        <input type="submit" name="insert_product" class="btn btn-info mb-3 px-3" value="insert Product">
       </div>
 
 
