43e2da44421846524033cd9f883b8e6db92c00d7
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-10-08 11:50:40 +0300

    hashing the password and checking if the user have some items in the cart after registrarion

--- users/checkout.php
+++ users/checkout.php
@@ -69,7 +69,7 @@ include('../includes/header.php');
           if (!isset($_SESSION['username'])) {
             include('./user_login.php');
           } else {
-            include('payment.php');
+            include('../payment.php');
           }
 
 
--- users/user_registration.php
+++ users/user_registration.php
@@ -87,6 +87,7 @@ if (isset($_POST['user_register'])) {
   $user_username = $_POST['user_username'];
   $user_email = $_POST['user_email'];
   $user_password = $_POST['user_password'];
+  $hash_password = password_hash($user_password, PASSWORD_DEFAULT);
   $conf_user_password = $_POST['conf_user_password'];
   $user_address = $_POST['user_address'];
   $user_contact = $_POST['user_contact'];
@@ -106,7 +107,7 @@ if (isset($_POST['user_register'])) {
     // Insert Query
     move_uploaded_file($user_image_tmp, "./user_images/$user_image");
     $insert_query = "insert into `user_table` (username,user_email,user_password,user_image,user_ip,user_address,user_mobile) 
-  values ('$user_username','$user_email','$user_password','$user_image','$user_ip','$user_address','$user_contact')";
+  values ('$user_username','$user_email','$hash_password','$user_image','$user_ip','$user_address','$user_contact')";
 
     $sql_execute = mysqli_query($con, $insert_query);
 
@@ -116,7 +117,16 @@ if (isset($_POST['user_register'])) {
       die(mysqli_error($con));
     }
   }
-
-
+  // Selecting cart items
+  $select_cart_items = "select * from `cart_details` where ip_address='$user_ip'";
+  $result_cart = mysqli_query($con, $select_cart_items);
+  $rows_count = mysqli_num_rows($result_cart);
+  if ($rows_count > 0) {
+    $_SESSION['username'] = $user_username;
+    echo "<script>alert('You have items in your cart.!!');</script>";
+    echo "<script>window.open('checkout.php','_self');</script>";
+  } else {
+    echo "<script>window.open('../index.php','_self');</script>";
+  }
 }
 ?>
